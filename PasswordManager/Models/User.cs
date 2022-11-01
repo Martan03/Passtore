ea82693ac85d105f3b46bf027cf86ed027681d14
Martan03 <martinslezak03@gmail.com>
2022-11-01 19:32:19 +0100

    Add user

---
+++ PasswordManager/Models/User.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+
+namespace PasswordManager.Models
+{
+    internal class User
+    {
+        private string _filePath { get; set; } = Path.Join(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Passtore/user-info.json");
+        [JsonProperty]
+        private string _password { get; set; }
+
+        public User LoadFromJson()
+        {
+            if (!File.Exists(_filePath))
+                return null;
+            return JsonConvert.DeserializeObject<User>(_filePath) ?? null;
+        }
+
+        public void SaveToJson()
+        {
+            FileInfo fi = new(_filePath);
+            if (!fi.Directory!.Exists)
+                Directory.CreateDirectory(_filePath);
+            File.WriteAllText(_filePath, JsonConvert.SerializeObject(this));
+        }
+
+        public bool CheckPassword(string password)
+        {
+            return string.Equals(_password, HashPassword(password));
+        }
+
+        private string HashPassword(string password)
+        {
+            byte[] salt = RandomNumberGenerator.GetBytes(128 / 8);
+            return Convert.ToBase64String(KeyDerivation.Pbkdf2(
+                password: password,
+                salt: salt,
+                prf: KeyDerivationPrf.HMACSHA256,
+                iterationCount: 100000,
+                numBytesRequested: 256 / 8
+            ));
+        }
+    }
+}
--- PasswordManager/Views/LoginPage.xaml.cs
+++ PasswordManager/Views/LoginPage.xaml.cs
@@ -1,3 +1,5 @@
+using System.Security.Cryptography;
+
 namespace PasswordManager.Views;
 
 public partial class LoginPage : ContentPage
@@ -6,4 +8,10 @@ public partial class LoginPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+	private void LoginButton_Clicked(object sender, EventArgs e)
+	{
+        var pbkdf2 = new Rfc2898DeriveBytes(password, salt, 100000);
+        byte[] hash = pbkdf2.GetBytes(20);
+    }
 }
